78b57e94a25390d717c1781415c345bcbd8973b3
Marthyn Olthof <Marthyn@live.nl>
2014-12-18 12:38:45 +0100

    Add logo to client model

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "rails-assets-chartjs", "~> 1.0.1.beta.4"
 gem "twitter-text" # hashtag parsing
 gem "jquery-atwho-rails" # autocomplete
 gem "haml-rails"
+gem "paperclip", "~> 4.2"
 
 # caching
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217165450) do
+ActiveRecord::Schema.define(version: 20141218111720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,12 @@ ActiveRecord::Schema.define(version: 20141217165450) do
   end
 
   create_table "clients", force: true do |t|
-    t.string "name",        default: "", null: false
-    t.string "description", default: ""
+    t.string   "name",              default: "", null: false
+    t.string   "description",       default: ""
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   create_table "delayed_jobs", force: true do |t|
