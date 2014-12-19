041a918bdcf1aa20dfcee654ce4a4ae5dca6ba8e
Marthyn Olthof <Marthyn@live.nl>
2014-12-19 12:05:19 +0100

    Fix timeseries for new EntryStats

--- config/routes.rb
+++ config/routes.rb
@@ -17,17 +17,16 @@ Hours::Application.routes.draw do
     resources :projects, only: [:index, :edit, :new, :update, :create, :show]
     resources :categories, only: [:index, :create, :edit, :update]
     resources :entries, only: [:create, :destroy, :update, :edit]
+
+    get "/users/:id(/:time_span)" => "users#show", as: "user"
     resources :users, only: [:index, :update] do
       resources :entries, only: :index
     end
-
-    get "/users/:id(/:time_span)" => "users#show", as: "user"
 
     resources :tags, only: [:show]
     resources :clients, only: [:show, :index, :edit, :update, :create]
 
     get "user/edit" => "users#edit", as: :edit_user
-
     get "account/edit" => "accounts#edit", as: :edit_account
     delete "account" => "accounts#destroy", as: :destroy_account
   end
